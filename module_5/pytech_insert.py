7d00f922cdce18e8cf0a85cae4e4efa0df040f61
johwall1 <johwall1@my365.bellevue.edu>
2022-07-10 21:03:59 -0400

    revised

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,39 +1,39 @@
 from pymongo import MongoClient
 
-url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
+url = "mongodb+srv://admin:admin@cluster0.jscwh.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(url)
 
 db = client.pytech
 
-jane = {
+luke = {
     "student_id": "1007",
-    "first_name": "Jane",
-    "last_name": "Doe",
+    "first_name": "Luke",
+    "last_name": "Skywalker",
 }
 
-john = {
+darth = {
     "student_id": "1008",
-    "first_name": "John",
-    "last_name": "Doe",
+    "first_name": "Darth",
+    "last_name": "Vader",
 }
 
-johnny = {
+ahsoka = {
     "student_id": "1009",
-    "first_name": "Johnny",
-    "last_name": "Test",
+    "first_name": "Ahsoka",
+    "last_name": "Tano",
 }
 
 students = db.students
 
 print("\n  -- INSERT STATEMENTS --")
-jane_student_id = students.insert_one(jane).inserted_id
-print("  Inserted student record Jane Doe into the students collection with document_id " + str(jane_student_id))
+luke_student_id = students.insert_one(luke).inserted_id
+print("  Inserted student record Jane Doe into the students collection with document_id " + str(luke_student_id))
 
-john_student_id = students.insert_one(john).inserted_id
-print("  Inserted student record John Doe into the students collection with document_id " + str(john_student_id))
+darth_student_id = students.insert_one(darth).inserted_id
+print("  Inserted student record John Doe into the students collection with document_id " + str(darth_student_id))
 
-johnny_student_id = students.insert_one(johnny).inserted_id
-print("  Inserted student record Johnny Test into the students collection with document_id " + str(johnny_student_id))
+ahsoka_student_id = students.insert_one(ahsoka).inserted_id
+print("  Inserted student record Johnny Test into the students collection with document_id " + str(ahsoka_student_id))
 
 input("\n\n  End of program, press any key to exit... ")
